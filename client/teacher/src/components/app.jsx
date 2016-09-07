295a8b7d8ca3dab88d8f1672249ed131c6e606c2
Damien Rochford <djrochford@gmail.com>
2016-09-07 11:20:34 -0700

    Teacher view skelton; working question POST

---
+++ client/teacher/src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+var App = (props) => (
+  <div className="grid">
+    <div className="row header">
+      <h1>Smart Questions</h1>
+    </div>
+  </div>
+);
+
+export default App;
+
+      // <ul className="nav">
+      //   <li><Link to="/questions">Questions</Link></li>
+      //   <li><Link to="/students">Students</Link></li>
+      // </ul>
---
+++ client/teacher/src/components/questions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+var Questions = () => (
+  <div className="col-6 questions-container">
+    <div className="question-list-container">
+    </div>
+</div>
+);
+
+export default Questions;
