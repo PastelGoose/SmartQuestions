4e5a404ed3a613859bc8fde3f3a91bf225f12d20
Damien Rochford <djrochford@gmail.com>
2016-09-11 16:26:17 -0700

    Add Stephen's code for StudentCompetence component.

--- client/teacher/src/components/StudentCompetency.jsx
+++ client/teacher/src/components/StudentCompetency.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
+import {core as Core} from 'zingchart-react';
 
-const StudentCompetency = (props) => (
-  <div>
-    <h3>Inside Student Competency Component</h3>
-    {props.competency.map((category) => (
-      <ul key={category.categoryId}>
-        <li>Category: {category.categoryName}</li>
-        <li>Competency Score: {category.competencyScore}</li>
-        <li>Improving: {category.isImproving.toString()}</li>
-      </ul>
-    ))}
-  </div>
-);
+class StudentCompetency extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
+
+  loadRadarChart() {
+    // Best viewed with at least 3 categories
+    var categories = [];
+    var scores = [];
+    this.props.competency.forEach((category) => {
+      categories.push(category.categoryName);
+      scores.push(category.competencyScore);
+    });
+
+    var competencyChart = {
+      type: 'radar',
+      plot: {
+        aspect: 'area'
+      },
+      'scale-k': {
+        labels: categories
+      },
+      series: [{values: scores}]
+    };
+
+    zingchart.render({
+      id: 'competency-chart',
+      width: 600,
+      height: 400,
+      data: competencyChart
+    });
+  }
+
+  render() {
+
+    this.loadRadarChart();
+
+    return (
+      <div>
+        <div id="competency-chart"></div>
+      </div>
+
+    );
+    
+  }
+}
+    
 export default StudentCompetency;
